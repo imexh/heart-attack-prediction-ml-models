9652829991868511c209786618f78be75e000fd5
imexh <chandikaranawella@gmail.com>
2024-06-18 22:35:32 +0530

    refactoring

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pandas as pd
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from tensorflow.keras.models import load_model
-from datetime import datetime, timedelta
 
 app = Flask(__name__)
 CORS(app)
